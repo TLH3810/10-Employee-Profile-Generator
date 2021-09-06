67e2d801d67bb7984a16f3e70b139ffcc330572e
Yorkie Fan <terahowell@gmail.com>
2021-09-06 18:11:17 -0400

    moved folders out of development so that tests can run

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,8 @@
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+
+describe("Manager Tests ", ()=>{
+    it ("Can set the office number")
+    const testValue = 83;
+    const result = new Manager()
+})
