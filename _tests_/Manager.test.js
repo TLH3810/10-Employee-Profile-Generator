89ed9be47145d1bd9781703f3b90d9debe0f9df4
Yorkie Fan <terahowell@gmail.com>
2021-09-12 18:46:21 -0400

    adding tests for Manager

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,8 +1,21 @@
 const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
+
+//write tests for both the getRole and getOfficeNumber funcitons from Manager.js
 describe("Manager Tests ", ()=>{
-    it ("Can set the office number")
-    const testValue = 83;
-    const result = new Manager()
-})
+    describe("managerInfo",()=>{
+        it ("return manager info", ()=>{
+            const testValue = "Manager";
+            const result = new Manager("Smith",1,"test1@test1.com",83);
+            expect(result.getRole()).toBe(testValue);
+        });
+    });
+    describe("officeNumber", ()=> {
+        it ("Can set the office number", () => {
+            const testValue = 83;
+            const result = new Manager("Smith",1,"test1@test1.com",testValue);
+            expect(result.officeNumber).toequal(testValue);
+        });
+    });
+});
